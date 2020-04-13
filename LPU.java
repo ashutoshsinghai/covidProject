53ba2b643926ce9ccc3f9b85c482a5d6c37e6fc1
Ashutosh <ashutosh.empli5@gmail.com>
2020-04-13 10:27:23 +0530

    added index page and countries page

---
+++ LPU.java
@@ -0,0 +1,114 @@
+import java.util.*;
+
+import java.time.LocalDate;
+interface Ugc
+{
+    int getadmission()
+    {
+        Scanner ip=new Scanner(System.in);
+        System.out.println("enter percentage of student");
+        int per=ip.nextint();
+        if(per>=60)
+        {
+            int reg_no=ip.nextint();
+            return(reg_no);
+        }
+    else
+{
+    System.out.println("Not eligible");
+    return 0;
+}
+}
+abstract Stringpayfee();
+}
+interface Aicte
+{
+    getadmission()
+    {
+        date()
+        {
+            LocalDate date= LocalDate.of(2020,4,9.plusDays(5);
+            System.out.println("couselling date="+date);
+            
+        }
+        int token_no()
+        Scanner ip=new Scanner(System.in);
+        int token_no=ip nextint();
+        return(token_no);
+    }
+    disp couselling_token_no();
+    System.out.System.out.println("couselling_token_no="+token_no);
+ }
+ }
+ University implements Ugc,Aicte
+ {
+     Scanner ip=new Scanner(System.in);
+     int fee_due;
+     String payfee()
+     {
+         int fee=25000;
+         System.out.println("fee="+fee);
+     }
+     Override() 
+     {
+         System.out.println("enter 1 for direct addmission throughing and 2 for Aicte");
+         int ch=ip.nextint();
+         if(ch==1)
+         {
+             int fee=25000;
+             int getadmission()
+             {
+                 System.out.println("enter percentage");
+                 int t=ip.nextint();
+                 if(t>=40)
+                 {
+                     int reg_no=ip.nextint();
+                     return (reg_no) ;
+                 }
+                 else{
+                     return 0;
+                     
+                 }
+                int fee=25000;
+             }
+             else if(ch==2)
+             {
+                 int fee=34000;
+                 Ugc.u=new Ugc();
+                 int r=u.getadmission();
+                 System.out.println("regestration no="+r);
+                 Aicte.a=newAicte();
+                 a.getadmission();
+             }
+             else
+             {
+                 System.out.println("Better luck next time");
+                 
+             }
+             Lpu extends University
+             {
+                 Scanner ip=new Scanner(System.in);
+                 Lpu l=new Lpu();
+                 l.Override();
+                 due_fee()
+                 {
+                     System.out.println("enter paid fee");
+                     int paid_fee=ip.nextint();
+                     int reg_no=ip.nextint();
+                     fee=fee-paid_fee;
+                     System.out.println("due fee="+fee);
+                     System.out.println("recept no="+recept_no);
+                 }
+             }
+             class main
+              public static void main (String[] args) {
+                  /* code */
+              Lpu.t=new Lpu();
+              t.getadmission();
+              t.Override();
+              t.due_fee();
+             }
+                 /* code */
+             }
+             
+  );
